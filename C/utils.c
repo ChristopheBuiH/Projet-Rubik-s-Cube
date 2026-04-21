90d3e90b9f8de243e5328eb756305ccf205a6455
Eloi-Menpiot <eloi.menpiot@gmail.com>
2026-04-21 15:10:51 +0200

    Version finale

--- C/utils.c
+++ C/utils.c
@@ -15,7 +15,7 @@
 // Taille de la plage mémoire à mapper
 #define LWHPS2FPGA_SPAN 0x00200000
 // Offset de l'IP dans la mémoire
-#define IP_OFFSET 0x00001000
+#define IP_OFFSET 0x00020000
 
 uint32_t convertisseur32bits(const char *binaire) {
     uint32_t motbits = 0;
@@ -40,7 +40,7 @@ void separateur(const char *str, uint8_t resultat[32]) {
             listemots[i][j] = str[(i * width) + j];
         }
         listemots[i][width] = '\0';
-        resultat[i] = convertisseur32bits(listemots[i]);
+        resultat[i] = (uint8_t)convertisseur32bits(listemots[i]);
     }
 
     for (int i = nb_mots; i < 32; i++) {
@@ -48,41 +48,69 @@ void separateur(const char *str, uint8_t resultat[32]) {
     }
 }
 
-/*int Recuperateur(const char *chemin, char *buffer, size_t bufferSize) {
+int Recuperateur(const char *jarPath, const char *inputCube, char *buffer, size_t bufferSize) {
     FILE *fp;
-    char commande[512];
+    char commande[1024];
 
-    snprintf(commande, sizeof(commande), "java -jar \"%s\"", chemin);
+    // On construit la commande : java -jar RubikSolver.jar [input]
+    snprintf(commande, sizeof(commande), "java -jar %s \"%s\"", jarPath, inputCube);
 
-    fp = _popen(commande, "r");
-
-    // Gestion erreurs
+    fp = popen(commande, "r"); 
     if (fp == NULL) {
-        _wperror(L"Erreur");
+        perror("Erreur lors de l'exécution du JAR");
         return 1;
     }
 
-    while (fgets(buffer, bufferSize, fp) != NULL) {
-        printf("Sortie Java : %s", buffer);
+    if (fgets(buffer, bufferSize, fp) != NULL) {
+        buffer[strcspn(buffer, "\r\n")] = '\0';
     }
 
-    // Nettoyer la dernière ligne lue
-    size_t blen = strcspn(buffer, "\r\n");
-    buffer[blen] = '\0';
-
-    _pclose(fp);
+    pclose(fp); 
     return 0;
 }
-*/
+const char* traduire_mouvement(uint8_t val) {
+    switch(val) {
+        case 0b110100: return "U";
+        case 0b111100: return "U2";
+        case 0b111000: return "U'";
+        
+        case 0b010100: return "R";
+        case 0b011100: return "R2";
+        case 0b011000: return "R'";
+        
+        case 0b100001: return "F";
+        case 0b100011: return "F2";
+        case 0b100010: return "F'";
+        
+        case 0b110001: return "D";
+        case 0b110011: return "D2";
+        case 0b110010: return "D'";
+        
+        case 0b010001: return "L";
+        case 0b010011: return "L2";
+        case 0b010010: return "L'";
+        
+        case 0b100100: return "B";
+        case 0b101100: return "B2";
+        case 0b101000: return "B'";
+        
+        default: return "";
+    }
+}
 
 int main() {
-    char buffer[256];
+    char input[256];
+    char buffer[1024];
     uint8_t resultat[32] = {0x0};
 
-    // int chaine = Recuperateur("C:\\Travail\\ENSEA\\2A\\Projet\\SaintGraal\\RUBIKSCUBESOLVER_Final\\RubikSolver.jar", buffer, sizeof(buffer));
-    separateur("010001010100011011010001", resultat);
+    printf("Entrez le cube ou le scramble : ");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\r\n")] = '\0';
 
+    int truc = Recuperateur("~/RUBIKSCUBESOLVER_FINAL.jar", input, buffer, sizeof(buffer));
+    separateur(buffer, resultat);
 
+    /*
     //Mon mémoire
     int fd;
     void *virtual_base;
@@ -115,21 +143,51 @@ int main() {
     printf("Écriture\n");
     fflush(stdout);
 
+    
     uint32_t valeur = *reg_fpga;
     printf("Valeur lue : 0x%08X\n", valeur);
+    
 
-    /*
+    
     for (int i = 0; i < 32; i++) {
         *(reg_fpga + i) = resultat[i];
     }
-    */
+    
 
     // On libère la mémoire mappée et on ferme le descripteur de fichier
     if (munmap(virtual_base, LWHPS2FPGA_SPAN) != 0) {
         perror("ERREUR: flop de munmap");
     }
     close(fd);
+    */
 
+    FILE *fichier = fopen("instructions.txt", "w");
+
+    printf("\nInstructions :\n");
+
+    for (int i = 0; i < 32; i++) {
+        if (resultat[i] != 0) {
+            
+            for (int j = 5; j >= 0; j--) {
+                fprintf(fichier, "%c", (resultat[i] & (1 << j)) ? '1' : '0');
+            }
+            fprintf(fichier, "\n"); // Un mouvement par ligne
+
+            
+            for (int j = 5; j >= 0; j--) {
+                printf("%c", (resultat[i] & (1 << j)) ? '1' : '0');
+            }
+            printf("\n");
+        }
+    }
+
+    printf("\nSéquence pour le robot : ");
+    for (int i = 0; i < 32; i++) {
+        if (resultat[i] != 0) {
+            printf("%s ", traduire_mouvement(resultat[i]));
+        }
+    }
+    printf("\n\n");
 
     return 0;
 }
